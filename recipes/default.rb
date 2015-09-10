c5b4d8b03568dc69aa186fd5321ce2c5f6d0de25
ashvarts <ashvarts@gmail.com>
2015-09-10 11:32:25 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,5 +3,5 @@ databag = Chef::EncryptedDataBagItem.load("credentials", "sysward")
 api_key = databag["api_key"]
 
 execute "Install Sysward" do
-  command "curl -sSL https://raw.githubusercontent.com/sysward/sysward-agent/master/install.sh | API_KEY=#{api_key} bash"
+  command "curl -sSL https://sysward.com/install.sh | API_KEY=#{api_key} bash"
 end
